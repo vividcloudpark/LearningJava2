a73e8782036bf1ad262fc7fcc42f07d46708858f
projector-user <vividxenon@gmail.com>
2023-01-01 07:08:19 +0000

    주문 할인 도메인 개발

---
+++ src/main/java/hello/core/discount/DiscountPolicy.java
@@ -0,0 +1,14 @@
+package hello.core.discount;
+
+import hello.core.member.Member;
+
+public interface DiscountPolicy {
+    /*
+    Return 할인대상금액
+
+    */
+
+    int discount(Member member, int price);
+
+
+}
