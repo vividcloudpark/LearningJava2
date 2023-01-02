f0ab0620f891f6eeec13023838572b1a06bf0c45
projector-user <vividxenon@gmail.com>
2023-01-02 06:27:25 +0000

    AppConfig 리팩토링

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,8 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -10,11 +12,20 @@ import hello.core.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }//생성자주입 생성자를통해 인스턴스를주입 (Injection)
 
-    public OrderService orderService(){//의존관계를 외부에서 주입해주는것처럼행동  이것이  바로 의존성주입
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
+
+    public OrderService orderService(){//의존관계를 외부에서 주입해주는것처럼행동  이것이  바로 의존성주입
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
+    }
+
 
 }
