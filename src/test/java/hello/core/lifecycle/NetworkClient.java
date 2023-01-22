ca2ab91061fc7319a55b7343a8ac693b1fe2cb6f
cloud.park <vividxenon@gmail.com>
2023-01-22 23:44:11 +0900

    Spring Bean 사용 - PostConstruct, PreDestroy사용

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,9 +1,11 @@
 package hello.core.lifecycle;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
-public class NetworkClient implements InitializingBean, DisposableBean {
+public class NetworkClient {
     private String url;
 
     public NetworkClient() {
@@ -32,14 +34,14 @@ public class NetworkClient implements InitializingBean, DisposableBean {
 
 
     //의존관계 주입이 끝나고 불러줌
-    @Override
-    public void afterPropertiesSet() throws Exception {
+    @PostConstruct
+    public void init() {
         connect();
         call("초기화 연결 메시지");
     }
 
-    @Override
-    public void destroy() throws Exception {
+    @PreDestroy
+    public void close(){
         disconnect();
     }
 }
