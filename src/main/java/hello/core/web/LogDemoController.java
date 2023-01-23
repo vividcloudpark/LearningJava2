013241cdf26edafd16c4c4a40ebf2677b8f4318b
cloud.park <vividxenon@gmail.com>
2023-01-23 15:48:32 +0900

    MyLogger 구현 - Request 스코프를 이용해서 Request가 일어날때만 생성/소멸되는 빈 만들기

---
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -0,0 +1,32 @@
+package hello.core.web;
+
+import hello.core.common.MyLogger;
+import jakarta.inject.Provider;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequiredArgsConstructor
+public class LogDemoController {
+
+    private final LogDemoService logDemoService;
+    private final Provider<MyLogger> myLoggerProvider; //고객이 나오기도 전에 (HTTP 요청이 오기도 전에) 생성자가 내놓으라고 하니까
+
+    @RequestMapping("log-demo")
+    @ResponseBody
+    public String logDemo(HttpServletRequest request) {
+        String requestURL = request.getRequestURL().toString();
+        MyLogger myLogger = myLoggerProvider.get();
+        myLogger.setRequestURL(requestURL);
+        myLogger.log("controller test");
+        logDemoService.logic("testId");
+        return "OK";
+    }
+
+
+
+}
